04f339d8f59fb0a281c70a71d4fd6b6dd987f61f
Ingmar <ingmar@Ingmars-Air.lan1>
2021-05-19 09:34:09 -0400

    added TV

--- colorchanger.js
+++ colorchanger.js
@@ -59,4 +59,11 @@ function getRandomColor(){
   setInterval("changeHeaderColor()",500);
   
   
+  var buttons = document.querySelectorAll('.television__channel a');
 
+  for(var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(e) {
+      document.querySelector('.television__screen iframe').src = this.href;
+      e.preventDefault();
+    });
+  }
